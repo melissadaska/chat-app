94baa99a8b98d998f89e8db1a21af0c5a96d2b93
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-10-19 08:02:52 -0500

    add dummy users

---
+++ src/Components/Users.js
@@ -0,0 +1,38 @@
+export const dummyUsers = [
+    {
+    id: "1",
+    name: "Chip The Chipmunk",
+    email: "chip@email.com",
+    photoUrl: "https://raw.githubusercontent.com/melissadaska/chat-app/main/src/assets/chip.png",
+    role: "Member",
+    info: "Master of hiding nuts",
+    welcomeMessage: "Hey, I would love to chat!"
+    },
+    {
+    id: "2",
+    name: "Barry The Bear",
+    email: "barry@email.com",
+    photoUrl: "https://raw.githubusercontent.com/melissadaska/chat-app/main/src/assets/barry.png",
+    role: "Member",
+    info: "Fish Catcher",
+    welcomeMessage: "Message me, I don't bite!"
+    },
+    {
+    id: "3",
+    name: "Dana The Dolphin",
+    email: "dana@email.com",
+    photoUrl: "https://raw.githubusercontent.com/melissadaska/chat-app/main/src/assets/dana.png",
+    role: "Member",
+    info: "Director of the Ocean",
+    welcomeMessage: "Come take a swim!"
+    },
+    {
+    id: "4",
+    name: "Harry The Horse",
+    email: "harry@email.com",
+    photoUrl: "https://raw.githubusercontent.com/melissadaska/chat-app/main/src/assets/harry.png",
+    role: "Member",
+    info: "Watcher of the Hay",
+    welcomeMessage: "Looking to connect!"
+    },
+    ]
