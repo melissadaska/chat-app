0310ab32d8b03250f623c0747b9157e142fc2ded
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-10-25 15:21:56 -0500

    add talkjs

--- src/Components/MyNetwork.js
+++ src/Components/MyNetwork.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Talk from 'talkjs';
 import { dummyUsers } from './Users';
 
 class MyNetwork extends Component {
@@ -13,6 +14,42 @@ class MyNetwork extends Component {
             currentUser
         }
     }
+
+    handleClick(userId) {
+        // Get two users that will be in conversation
+        const { currentUser } = this.state;
+        const user = dummyUsers.find(user => user.id === userId)
+
+        // session initialization code
+        Talk.ready
+        .then(() => {
+            // Create two users that will be in conversation
+            const me = new Talk.User(currentUser);
+            const other = new Talk.User(user)
+
+            // Create talk session if does not exist.
+            if (!window.talkSession) {
+                window.talkSession = new Talk.Session({
+                    appId: 'tg57V1o1',
+                    me: me
+                });
+            }
+
+            // Get conversation or create one
+            const conversationId = Talk.oneOnOneId(me, other);
+            const conversation = window.talkSession.getOrCreateConversation(conversationId);
+
+            // Set participants of conversation
+            conversation.setParticipant(me);
+            conversation.setParticipant(other);
+
+            // Create and mount chatbox in container
+            this.chatbox = window.talkSession.createChatbox(conversation);
+            this.chatbox.mount(this.container);
+        })
+        .catch(e => console.error(e));
+    }
+
     render() {
         const { currentUser } = this.state;
         return (
@@ -43,12 +80,15 @@ class MyNetwork extends Component {
                                     <p>{user.info}</p>
                                 </div>
                                 <div className='user-action'>
-                                <button>Message</button>
+                                <button onClick={(userId) => this.handleClick(user.id)}>Message</button>
                                 </div>
                             </div>
                           </li>  
                         )}
                     </ul>
+                    <div className="chatbox-container" ref={c => this.container = c}>
+                        <div id="talkjs-container" style={{height: "300px"}}><i></i></div>
+                    </div>
                 </div>
             </div>
         )
